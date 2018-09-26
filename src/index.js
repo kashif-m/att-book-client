ca1b1f38b4455efe1afc7f5f10876e0f6ac6211d
Kashif <mailmethis@protonmail.com>
2018-09-26 10:04:02 +0530

    INIT

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+// css
+import './styles/style.css'
+
+import App from './App'
+
+render(
+  <App />,
+  document.getElementById('react-app')
+)
